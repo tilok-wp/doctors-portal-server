883d84b7b21e83baf0f1200d8a0ce784cd9f7983
Tilok Paul <tilokpaul.web@gmail.com>
2022-05-18 03:43:47 +0600

    Create dashboard features API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access!' })
+    }
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.TOKEN_KEY_SECREATE, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access!' })
+        }
+        req.decoded = decoded
+        next()
+    });
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9dn9g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -77,11 +95,48 @@ async function run() {
             res.send({ success: true, booking: result })
         })
 
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyJWT, async (req, res) => {
             const email = req.query.email
-            const query = { paitentEmail: email }
-            const bookings = await bookingsCollection.find(query).toArray()
-            res.send(bookings)
+            // const authorization = req.headers.authorization
+            const decodedEmail = req.decoded.email
+            if (decodedEmail === email) {
+                const query = { paitentEmail: email }
+                const bookings = await bookingsCollection.find(query).toArray()
+                res.send(bookings)
+            } else {
+                return res.status(403).send({ message: 'Forbidden access!' })
+            }
+            // console.log(authorization)
+        })
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray()
+            res.send(users)
+        })
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+
+        })
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email
+            // console.log(req.params)
+            const requesterEmail = req.decoded.email
+            const requesterAccount = await userCollection.findOne({ email: requesterEmail })
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const updatedDoc = {
+                    $set: { role: 'admin' }
+                }
+                const result = await userCollection.updateOne(filter, updatedDoc,)
+                res.send(result)
+            } else {
+                res.status(403).send({ message: 'Forbidden access!' })
+            }
         })
 
         app.put('/user/:email', async (req, res) => {
@@ -93,7 +148,9 @@ async function run() {
                 $set: user
             }
             const result = await userCollection.updateOne(filter, updatedDoc, options)
-            res.send(result)
+            const token = jwt.sign({ email: email }, process.env.TOKEN_KEY_SECREATE, { expiresIn: '12h' })
+            // console.log(token)
+            res.send({ result, accessToken: token })
         })
     }
     finally {
@@ -111,5 +168,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port} `)
+    console.log(`Example app listening on port ${port}`)
 })
